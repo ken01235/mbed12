2a3b686bbc19de3a934037c1407c4646fa073ceb
ken01235 <kenkenrc08@gmail.com>
2021-05-26 03:04:40 -0700

    12_6

--- 12_6_use_servo_table/main.cpp
+++ 12_6_use_servo_table/main.cpp
@@ -37,7 +37,7 @@ int main() {
     while(1) {
 
     //TODO: revise this value according to your result
-    servo_control(37.222);
+    servo_control(37.2224000258);
 
     steps = 0;
     t.reset();
